f28bc46f765fa36cc8c26e6324ecf2696f103f80
xxbattousaixx <ohnoesninjas@gmail.com>
2021-04-08 16:59:26 -0400

    react

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "notes-app-uploads-dc7",
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://qpqlx8z6i9.execute-api.us-east-2.amazonaws.com/prod/",
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_iQgbkGRta",
+      APP_CLIENT_ID: "5s3vj1le0vkt0d4bhatgks09m5",
+      IDENTITY_POOL_ID: "c6193f46-a492-4b5a-9b6b-fefb9b5ddeab",
+    },
+  };
+  
+  export default config;
