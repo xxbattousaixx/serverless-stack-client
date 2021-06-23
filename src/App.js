50c367cde25dafddac7355498a4b033a8d6e8f4e
xxbattousaixx <dcarrassi@outlook.com>
2021-06-23 18:13:19 +0200

    title

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
         <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
           <LinkContainer to="/">
             <Navbar.Brand className="font-weight-bold text-muted">
-              Scratch
+              DC7 Notes
             </Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle />
